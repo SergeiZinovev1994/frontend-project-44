df7a2c58839c6e78aa73c3401f2ff8d7f6e6a497
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-03 17:10:02 +0300

    add brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,23 @@
+#! user/bin env node
+
+import { userName } from "./brain-games.js";
+import { expression, gameCalc } from "../src/cli.js";
+
+console.log(`What is the result of the expression?`);
+
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let a = Math.ceil(Math.random() * 10);
+let b = Math.ceil(Math.random() * 10);
+const operand = ['+', '*', '-'];
+const randomOperand = operand[getRndInteger(0, 2)];
+const express = a + randomOperand + b;
+
+const valueExpress = eval(express);
+
+
+const userAnswer = expression(express);
+
+gameCalc(userAnswer, express, valueExpress, userName);
--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,23 @@
 import readlineSync from '../readline-sync/lib/readline-sync.js';
 
 export const question = () => {
-  const question = readlineSync.question('May I have your name?');
+  const question = readlineSync.question('May I have your name? ');
   return question;
 };
 
 export const welcome = (userName) => console.log(`Hello, ${userName}!`);
 
 export const isEven = (number) => {
-  const question = readlineSync.question(`Question: ${number}`);
+  const question = readlineSync.question(`Question: ${number} `);
   return question;
 };
 
-export const gameOver = (userAnswer, number = 1, userName = 'Jho', count = 0) => {
+export const expression = (exp) => {
+  const question = readlineSync.question(`Question: ${exp} `);
+  return question;
+};
+
+export const gameEven = (userAnswer, number = 1, userName = 'Jho', count = 0) => {
   let rightAnswer = number % 2 === 0 ? 'yes' : 'no';
 
   if (userAnswer !== rightAnswer) {
@@ -24,6 +29,29 @@ export const gameOver = (userAnswer, number = 1, userName = 'Jho', count = 0) =>
     }
     number = Math.round(Math.random() * 1000);
     userAnswer = isEven(number);
-    gameOver (userAnswer, number, userName, count += 1)
+    gameEven (userAnswer, number, userName, count += 1)
+  }
+}
+
+export const gameCalc = (userAnswer, express, valueExpress, userName = 'Jho', count = 0) => {
+  const result = +userAnswer || userAnswer;
+  if (valueExpress !== result) {
+    return console.log(`${result} is wrong answer ;(. Correct answer was ${valueExpress}`);
+  } else {
+    console.log(`Your answer : ${userAnswer}\nCorrect!`);
+    if (count >= 2) {
+      return console.log(`Congratulations, ${userName}!`);
+    }
+    const getRndInteger = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+    let a = Math.ceil(Math.random() * 10);
+    let b = Math.ceil(Math.random() * 10);
+    const operand = ['+', '*', '-'];
+    const randomOperand = operand[getRndInteger(0, 2)];
+    express = a + randomOperand + b;
+    const valueExpress = eval(express);
+    userAnswer = expression(express);
+    gameCalc(userAnswer, express, valueExpress, userName, count += 1);
   }
 }
