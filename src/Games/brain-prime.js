84b716cd48245380e2334f37da7c8eb1cc2de041
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-04 16:09:09 +0300

    add src/Games/brain-prime.js

---
+++ src/Games/brain-prime.js
@@ -0,0 +1,30 @@
+import readlineSync from "../readline-sync/lib/readline-sync.js";
+
+const getNumber = (number) => {
+  const num = readlineSync.question(`Question: ${number} `);
+  return num;
+};
+
+export const prime = (userName, count = 0) => {
+  let randomNumber = Math.ceil(Math.random() * 100);
+  const userAnswer = getNumber(randomNumber);
+  const result = +userAnswer || userAnswer;
+  let rightAnswer = 'yes';
+  let i = 2;
+  while (i <= randomNumber / 2) {
+    if (randomNumber % i === 0) {
+      rightAnswer = 'no';
+      break;
+    }
+    i += 1;
+  }
+  if (result !== rightAnswer) {
+    return console.log(`Your answer: ${userAnswer}\n'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
+  } else {
+    console.log(`Your answer: ${userAnswer}\nCorrect!`);
+    if (count >= 2) {
+      return console.log(`Congratulations, ${userName}!`);
+    }
+    prime(userName, count += 1);
+  }
+};
