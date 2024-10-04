fde2f8883e5eba42d5fc3be8cd8e3bc9781f5c9a
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-04 10:39:31 +0300

    add function brain-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+import { userName } from "./brain-games.js";
+
+import { gameGCD } from "../src/cli.js";
+
+
+gameGCD(userName);
--- src/cli.js
+++ src/cli.js
@@ -17,6 +17,11 @@ export const expression = (exp) => {
   return question;
 };
 
+export const getDividsor = (a, b) => {
+  const dividsor = readlineSync.question(`Question: ${a} ${b} `);
+  return dividsor;
+}
+
 export const gameEven = (userAnswer, number = 1, userName = 'Jho', count = 0) => {
   let rightAnswer = number % 2 === 0 ? 'yes' : 'no';
 
@@ -45,13 +50,36 @@ export const gameCalc = (userAnswer, express, valueExpress, userName = 'Jho', co
     const getRndInteger = (min, max) => {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     };
-    let a = Math.ceil(Math.random() * 100);
-    let b = Math.ceil(Math.random() * 100);
+    let a = Math.ceil(Math.random() * 10);
+    let b = Math.ceil(Math.random() * 10);
     const operand = ['+', '*', '-'];
     const randomOperand = operand[getRndInteger(0, 2)];
     express = a + randomOperand + b;
     const valueExpress = eval(express);
     userAnswer = expression(express);
     gameCalc(userAnswer, express, valueExpress, userName, count += 1);
+  }
+}
+
+export const gameGCD = (userName, count = 0) => {
+  let numberA = Math.ceil(Math.random() * 10);
+  let numberB = Math.ceil(Math.random() * 10);
+  const userAnswer = getDividsor(numberA, numberB);
+  const resultUserAnswer = +userAnswer || userAnswer;
+  const smallerNumber = numberA >= numberB ? numberB : numberA;
+  let dividsor = 0;
+  for (let i = 0; i <= smallerNumber; i += 1) {
+    if (numberA % i === 0 && numberB % i === 0) {
+      dividsor = i;
+    }
+  }
+  if (resultUserAnswer !== dividsor) {
+    return console.log(`Your answer: ${userAnswer}\n'${userAnswer}' is wrong answer ;(. Correct answer was '${dividsor}'.\nLet's try again, ${userName}!`);
+  } else {
+    console.log(`Your answer: ${userAnswer}\nCorrect!`);
+      if (count >= 2) {
+        return console.log(`Congratulations, ${userName}!`);
+      }
+      gameGCD(userName, count += 1);
   }
 }
