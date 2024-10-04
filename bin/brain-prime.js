bbfcfc9fb01d7c86e1035c25df841f67de707bde
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-04 16:15:22 +0300

    update bin/brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { userName } from "./brain-games.js";
-import { prime } from "../src/cli.js";
+import { prime } from "../src/Games/brain-prime.js";
 
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
