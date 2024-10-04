0222734b848a3fad0483d9961f916c55676d638e
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-04 16:13:30 +0300

    update bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,20 +1,8 @@
 #!/usr/bin/env node
 
 import { userName } from "./brain-games.js";
-import { expression, gameCalc } from "../src/cli.js";
+import { gameCalc } from "../src/Games/brain-calc.js";
 
 console.log(`What is the result of the expression?`);
 
-const getRndInteger = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const a = Math.ceil(Math.random() * 10);
-const b = Math.ceil(Math.random() * 10);
-const operand = ['+', '*', '-'];
-const randomOperand = operand[getRndInteger(0, 2)];
-const express = a + randomOperand + b;
-const valueExpress = eval(express);
-const userAnswer = expression(express);
-
-gameCalc(userAnswer, express, valueExpress, userName);
+gameCalc(userName);
