0b14b43739df4e48fd5f87c449e857b0b36ef3cb
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-04 09:00:01 +0300

    update bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -9,8 +9,8 @@ const getRndInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let a = Math.ceil(Math.random() * 100);
-let b = Math.ceil(Math.random() * 100);
+const a = Math.ceil(Math.random() * 100);
+const b = Math.ceil(Math.random() * 100);
 const operand = ['+', '*', '-'];
 const randomOperand = operand[getRndInteger(0, 2)];
 const express = a + randomOperand + b;
