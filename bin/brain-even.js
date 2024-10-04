00112f437fa2d809fa82cb1ae411d63749a02e13
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-04 16:13:55 +0300

    update bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { isEven, gameEven } from "../src/cli.js";
+import { gameEven } from "../src/Games/brain-even.js";
 import { userName } from "./brain-games.js";
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
