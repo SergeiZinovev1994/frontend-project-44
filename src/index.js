3049d18bcd3ccfffb727eb736c345398585c043a
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-05 10:56:26 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ export const quest = (number) => {
   return question;
 };
 export const welcome = (userName) => console.log(`Hello, ${userName}!`);
-export const getRndInteger = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+export const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 export const countRounds = 3;
