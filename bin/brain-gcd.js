ba9e562dcef8a878d08c28d0b60cfd3662185767
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-05 09:22:08 +0300

    update bin/brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -3,4 +3,6 @@
 import { userName } from "./brain-games.js";
 import { gameGCD } from "../src/Games/brain-gcd.js";
 
+console.log(`Find the greatest common divisor of given numbers.`);
+
 gameGCD(userName);
