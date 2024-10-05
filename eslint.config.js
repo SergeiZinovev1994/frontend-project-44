61942a9a0db6dc3404a200eb8854fd7b50e9dd47
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-05 11:29:08 +0300

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,8 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
-
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  { ignores: ["demo.cast"] }
+  { ignores: ['demo.cast'], }
 ];
