12799d5ae49801c6abba1da0ca58b93f3dc87210
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-09 17:12:25 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,17 @@ export const welcome = () => {
   return user;
 }
 
-
-
 export const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 export const countRounds = 3;
+
+export const gameTry = (userAnswer, rightValue) => {
+  let processedAnswer = +userAnswer || userAnswer;
+  if (userAnswer === '0') {
+    processedAnswer = 0;
+  }
+  return processedAnswer !== rightValue ? false : true;
+}
+
+export const gameOver = (userName, answer, exactResult) => console.log(`Your answer: ${answer}\n'${answer}' is wrong answer ;(. Correct answer was '${exactResult}'.\nLet's try again, ${userName}!`);
+
+export const victory = (userName, answer, exactResult) => console.log(`Congratulations, ${userName}!`);
