59d3384c342bb9aaac013b6823d8ddbf6ee744b2
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-09 13:36:26 +0300

    update and rename bin/brainGames.js

--- bin/brain-games.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { welcome } from '../src/index.js';
-
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-export default userName;
-welcome(userName);
---
+++ bin/brainGames.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { welcome } from '../src/index.js';
+
+welcome();
