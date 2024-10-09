f94e17b95bcc7f35e9a991a2bf946eee95cabdbf
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-09 17:12:05 +0300

    update src/Games/brainCalc.js

--- src/Games/brainCalc.js
+++ src/Games/brainCalc.js
@@ -1,15 +1,14 @@
 import {
-  countRounds,
+  gameOver,
   getRndInteger,
   quest,
   welcome,
+  victory,
+  countRounds,
+  gameTry
 } from '../index.js';
 
-const gameCalc = (userName = 'Jho', count = 0) => {
-  if (count === 0) {
-    userName = welcome();
-    console.log('What is the result of the expression?');
-  }
+const expression = () => {
   const maxNumber = 10;
   const a = getRndInteger(0, maxNumber);
   const b = getRndInteger(0, maxNumber);
@@ -17,33 +16,46 @@ const gameCalc = (userName = 'Jho', count = 0) => {
   const minIndexOfOperand = 0;
   const maxIndexOfOperand = operand.length - 1;
   const randomOperand = operand[getRndInteger(minIndexOfOperand, maxIndexOfOperand)];
-  const express = `${a} ${randomOperand} ${b}`;
-  const startValueExpress = (val1, mathSign, val2) => {
-    let result = 0;
-    switch (mathSign) {
-      case '+': result = val1 + val2;
-        break;
-      case '-': result = val1 - val2;
-        break;
-      case '*': result = val1 * val2;
-        break;
-      default: result = 0;
-    }
-    return result;
-  };
-  const userAnswer = quest(express);
-  let result = +userAnswer || userAnswer;
-  if (userAnswer === '0') {
-    result = 0;
-  }
-  if (startValueExpress(a, randomOperand, b) !== result) {
-    return console.log(`Your answer: ${result}\n'${result}' is wrong answer ;(. Correct answer was '${startValueExpress(a, randomOperand, b)}'.\nLet's try again, ${userName}!`);
-  }
-  console.log(`Your answer : ${userAnswer}\nCorrect!`);
-  if (count >= countRounds - 1) {
-    return console.log(`Congratulations, ${userName}!`);
-  }
-  const newCount = count + 1;
-  return gameCalc(userName, newCount);
+  const result = `${a} ${randomOperand} ${b}`;
+  return result;
 };
+
+const startValueExpress = (express) => {
+  const signs = express.split(' ');
+  const num1 = +signs[0];
+  const num2 = +signs[signs.length - 1];
+  const operand = signs[1];
+  let result = 0;
+  switch (operand) {
+    case '+': result = num1 + num2;
+      break;
+    case '-': result = num1 - num2;
+      break;
+    case '*': result = num1 * num2;
+      break;
+    default: result = 0;
+  }
+  return result;
+}
+
+const gameCalc = (userName = 'Jho', count = 0) => {
+  if (count === 0) {
+    userName = welcome();
+    console.log('What is the result of the expression?');
+  }
+  while (count <= (countRounds - 1)) {
+    const express = expression();
+    const userAnswer = quest(express);
+    const rightAnswer = startValueExpress(express);
+    let switcher = gameTry(userAnswer, rightAnswer);
+    if (!switcher) {
+      return gameOver(userName, userAnswer, rightAnswer);
+    }
+    console.log(`Your answer : ${userAnswer}\nCorrect!`);
+    count += 1;
+    if (count === 3) {
+      return victory(userName, userAnswer, rightAnswer);
+    }
+  }
+}
 export default gameCalc;
