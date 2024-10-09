cdca858fb5c4b3c20c03ff7d495138b1bb4a8333
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-09 13:36:59 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ export const quest = (number) => {
   const question = readlineSync.question(`Question: ${number} `);
   return question;
 };
-export const welcome = (userName) => console.log(`Hello, ${userName}!`);
+
+export const welcome = () => {
+  console.log('Welcome to the Brain Games!');
+  const user = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${user}!`);
+  return user;
+}
+
+
+
 export const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 export const countRounds = 3;
-
-export const randomNumber = (num) => Math.ceil(Math.random() * num);
