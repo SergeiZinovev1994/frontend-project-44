75b4080d1d1493d4a852c7edcc1274e373b3589f
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-12 14:02:56 +0300

    update src/Games/brainProgression.js

--- src/Games/brainProgression.js
+++ src/Games/brainProgression.js
@@ -1,40 +1,22 @@
-import readlineSync from 'readline-sync';
-import { getRndInteger, countRounds, welcome } from '../index.js';
+import { welcome, gameCore } from '../index.js';
+import getRandomNumber from '../utils.js';
+import addProgression from '../addProgression.js';
 
-const getMissedValue = (arr) => {
-  const missedValue = readlineSync.question(`Question: ${arr} `);
-  return missedValue;
+const game = () => {
+  const minCountEl = 5;
+  const maxCountEl = 10;
+  const minStep = 1;
+  const maxStep = 20;
+  const progression = addProgression(minCountEl, maxCountEl, minStep, maxStep);
+  const somethingIndex = getRandomNumber(0, progression.length - 1);
+  const rightAnswer = progression[somethingIndex];
+  progression[somethingIndex] = '..';
+  return [progression, rightAnswer];
 };
 
-const gameProgression = (userName = 'Jho', count = 0) => {
-  if (count === 0) {
-    userName = welcome();
-    console.log('What number is missing in the progression?');
-  }
-  const progression = [];
-  const minEl = 5;
-  const maxEl = 10;
-  const countElements = getRndInteger(minEl, maxEl);
-  const maxNumber = 20;
-  const valueForStep = getRndInteger(0, maxNumber);
-  progression[0] = getRndInteger(0, maxNumber);
-  for (let i = 1; i < countElements; i += 1) {
-    progression[i] = progression[i - 1] + valueForStep;
-  }
-  const somethingValue = getRndInteger(0, countElements - 1);
-  const rightAnswer = progression[somethingValue];
-  progression[somethingValue] = '..';
-  const progressionInString = progression.join(' ');
-  const userAnswer = getMissedValue(progressionInString);
-  const result = +userAnswer || userAnswer;
-  if (result !== rightAnswer) {
-    return console.log(`Your answer: ${userAnswer}\n'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
-  }
-  console.log(`Your answer: ${userAnswer}\nCorrect!`);
-  if (count >= countRounds - 1) {
-    return console.log(`Congratulations, ${userName}!`);
-  }
-  const newCount = count + 1;
-  return gameProgression(userName, newCount);
-};
+const gameProgression = () => {
+  const userName = welcome();
+  console.log('What number is missing in the progression?');
+  gameCore(game, userName);
+}
 export default gameProgression;
