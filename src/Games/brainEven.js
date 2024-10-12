7c7f97a37845e90dc13f99982e9b905e39ae9a06
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-12 09:49:22 +0300

    update bin/Games/brainEven.js

--- src/Games/brainEven.js
+++ src/Games/brainEven.js
@@ -1,23 +1,15 @@
-import { countRounds, quest, welcome, getRndInteger } from '../index.js';
+import { gameCore, welcome, getRndInteger } from '../index.js';
 
-const gameEven = (userName = 'Jho', count = 0) => {
-  if (count === 0) {
-    userName = welcome();
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  }
+const game = () => {
   const maxNumber = 100;
   const randomNum = getRndInteger(0, maxNumber);
-  const userAnswer = quest(randomNum);
   const rightAnswer = randomNum % 2 === 0 ? 'yes' : 'no';
+  return [randomNum, rightAnswer];
+}
 
-  if (userAnswer !== rightAnswer) {
-    return console.log(`Your answer : ${userAnswer}\n'${userAnswer}' is wrong answer ;/(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
-  }
-  console.log(`Your answer : ${userAnswer}\nCorrect!`);
-  if (count >= countRounds - 1) {
-    return console.log(`Congratulations, ${userName}!`);
-  }
-  const newCount = count + 1;
-  return gameEven(userName, newCount);
+const gameEven = () => {
+  const user = welcome();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  gameCore(game, user);
 };
 export default gameEven;
