c77d985c308b430832b9b766374253145420be2e
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-16 17:40:52 +0300

    update src/games/brainEven.js

--- src/Games/brainEven.js
+++ src/Games/brainEven.js
@@ -1,17 +1,15 @@
-import { gameCore, welcome } from '../index.js';
+import { gameCore } from '../index.js';
 import getRandomNumber from '../utils.js';
 
+const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+
 const game = () => {
-  const maxNumber = 100;
-  const minNumber = 1;
-  const randomNum = getRandomNumber(minNumber, maxNumber);
-  const rightAnswer = randomNum % 2 === 0 ? 'yes' : 'no';
-  return [randomNum, rightAnswer];
+  const randomNum = getRandomNumber(1, 100);
+  const answer = randomNum % 2 === 0 ? 'yes' : 'no';
+  return [randomNum, answer];
 };
 
-const gameEven = () => {
-  const user = welcome();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  gameCore(game, user);
+const runGameEven = () => {
+  gameCore(game, task);
 };
-export default gameEven;
+export default runGameEven;
