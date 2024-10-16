e8b0101086d42a5145ecb325c0905c6ce05a891b
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-16 17:40:31 +0300

    update src/games/brainCalc.js

--- src/Games/brainCalc.js
+++ src/Games/brainCalc.js
@@ -1,47 +1,41 @@
-import { welcome, gameCore } from '../index.js';
+import { gameCore } from '../index.js';
 import getRandomNumber from '../utils.js';
 
-const expression = () => {
-  const maxNumber = 10;
-  const minNumber = 0;
-  const number1 = getRandomNumber(minNumber, maxNumber);
-  const number2 = getRandomNumber(minNumber, maxNumber);
-  const operator = ['+', '*', '-'];
-  const minIndexOfOperator = 0;
-  const maxIndexOfOperator = operator.length - 1;
-  const randomOperator = operator[getRandomNumber(minIndexOfOperator, maxIndexOfOperator)];
-  const result = `${number1} ${randomOperator} ${number2}`;
+const task = 'What is the result of the expression?';
+
+const expression = (num1, operator, num2) => {
+  const result = `${num1} ${operator} ${num2}`;
   return result;
 };
 
-const startValueExpress = (express) => {
-  const signs = express.split(' ');
-  const num1 = +signs[0];
-  const num2 = +signs[signs.length - 1];
-  const operand = signs[1];
+const calculate = (num1, num2, operator) => {
   let result = 0;
-  switch (operand) {
+  switch (operator) {
     case '+': result = num1 + num2;
       break;
     case '-': result = num1 - num2;
       break;
     case '*': result = num1 * num2;
       break;
-    default: throw new Error(`Unknown order state: '${express}'!`);
+    default: throw new Error(`unknown condition!`);
   }
   return result;
 };
 
-const game = () => {
-  const exp = expression();
-  const rightAnswer = startValueExpress(exp);
-  return [exp, rightAnswer];
+const getDataGame = () => {
+  const number1 = getRandomNumber(0, 10);
+  const number2 = getRandomNumber(0, 10);
+  const operator = ['+', '*', '-'];
+  const minIndexOfOperator = 0;
+  const maxIndexOfOperator = operator.length - 1;
+  const randomOperator = operator[getRandomNumber(minIndexOfOperator, maxIndexOfOperator)];
+  const exp = expression(number1, randomOperator, number2);
+  const answer = calculate(number1, number2, randomOperator);
+  return [exp, answer.toString()];
 };
 
 const runGameCalc = () => {
-  const user = welcome();
-  console.log('What is the result of the expression?');
-  return gameCore(game, user);
+  return gameCore(getDataGame, task);
 };
 
 export default runGameCalc;
