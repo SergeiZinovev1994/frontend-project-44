44ec7edf51f11b028866b1b5bdcc4f82eac4945c
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-16 17:41:33 +0300

    update src/games/brainProgression.js

--- src/Games/brainProgression.js
+++ src/Games/brainProgression.js
@@ -1,23 +1,31 @@
-import { welcome, gameCore } from '../index.js';
+import { gameCore } from '../index.js';
 import getRandomNumber from '../utils.js';
-import addProgression from '../addProgression.js';
+
+const task = 'What number is missing in the progression?';
+
+const createProgression = (minCountElements, maxCountElements) => {
+  const progression = [];
+  const countElements = getRandomNumber(minCountElements, maxCountElements);
+  const valueForStep = getRandomNumber(1, 20);
+  progression[0] = getRandomNumber(0, 10);
+  for (let i = 1; i < countElements; i += 1) {
+    progression[i] = progression[i - 1] + valueForStep;
+  }
+  return progression;
+};
 
 const game = () => {
-  const minCountEl = 5;
-  const maxCountEl = 10;
-  const minStep = 1;
-  const maxStep = 20;
-  const progression = addProgression(minCountEl, maxCountEl, minStep, maxStep);
-  const somethingIndex = getRandomNumber(0, progression.length - 1);
-  const rightAnswer = progression[somethingIndex];
-  progression[somethingIndex] = '..';
-  const stringProgression = progression.join(' ');
-  return [stringProgression, rightAnswer];
+  const minCountElements = 5;
+  const maxCountElements = 10;
+  const progression = createProgression(minCountElements, maxCountElements);
+  const indexOfRandomElement = getRandomNumber(0, progression.length - 1);
+  const answer = progression[indexOfRandomElement];
+  progression[indexOfRandomElement] = '..';
+  const progressionStringWithGap = progression.join(' ');
+  return [progressionStringWithGap, answer];
 };
 
-const gameProgression = () => {
-  const userName = welcome();
-  console.log('What number is missing in the progression?');
-  gameCore(game, userName);
+const runGameProgression = () => {
+  gameCore(game, task);
 };
-export default gameProgression;
+export default runGameProgression;
