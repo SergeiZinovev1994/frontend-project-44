774e79cee6ecd7f5e72ce18d34d6fe4d3811c35d
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-17 12:33:52 +0300

    update src/games/brainEven.js

--- src/Games/brainEven.js
+++ src/Games/brainEven.js
@@ -3,9 +3,11 @@ import getRandomNumber from '../utils.js';
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (number) => number % 2 === 0;
+
 const game = () => {
   const randomNum = getRandomNumber(1, 100);
-  const answer = randomNum % 2 === 0 ? 'yes' : 'no';
+  const answer = isEven(randomNum) ? 'yes' : 'no';
   return [randomNum, answer];
 };
 
