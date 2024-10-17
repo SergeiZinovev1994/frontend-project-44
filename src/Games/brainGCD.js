7f9ce7490c73177c04d5d7d7c8f82890db8d72da
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-17 12:33:14 +0300

    update src/games/brainGCD.js

--- src/Games/brainGCD.js
+++ src/Games/brainGCD.js
@@ -5,13 +5,13 @@ const task = 'Find the greatest common divisor of given numbers.';
 
 const searchDividsor = (num1, num2) => {
   const smallerNumber = num1 >= num2 ? num2 : num1;
-  let dividsor = 1;
+  let divisor = 1;
   for (let i = 2; i <= smallerNumber; i += 1) {
     if (num1 % i === 0 && num2 % i === 0) {
-      dividsor = i;
+      divisor = i;
     }
   }
-  return dividsor;
+  return divisor;
 };
 
 const game = () => {
