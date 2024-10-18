214f1e8307e0595adea280f9bf241ee7dc07610b
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-10-18 10:21:22 +0300

    update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-    console.log('Welcome to the Brain Games!');
-    const user = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${user}!`);
+  console.log('Welcome to the Brain Games!');
+  const user = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${user}!`);
 };
